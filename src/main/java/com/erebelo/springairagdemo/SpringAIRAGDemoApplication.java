697562ac806b53a237c346b662efb94a3c3d524e
erebelo <eduldrebelo@gmail.com>
2025-01-27 13:50:59 -0300

    First commit

---
+++ src/main/java/com/erebelo/springairagdemo/SpringAIRAGDemoApplication.java
@@ -0,0 +1,13 @@
+package com.erebelo.springairagdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAIRAGDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAIRAGDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/erebelo/springairagdemo/SpringAIRAGDemoApplicationTest.java
@@ -0,0 +1,13 @@
+package com.erebelo.springairagdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAIRAGDemoApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
