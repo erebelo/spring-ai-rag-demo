d7ca63d31b6160fa165545d58ed2285d0a46cc3b
erebelo <eduldrebelo@gmail.com>
2025-02-18 16:26:16 -0300

    Adding ReponseStatus annotation in place of ResponseEntity

--- src/main/java/com/erebelo/springairagdemo/controller/NBAController.java
+++ src/main/java/com/erebelo/springairagdemo/controller/NBAController.java
@@ -8,11 +8,12 @@ import com.erebelo.springairagdemo.model.Question;
 import com.erebelo.springairagdemo.service.NBAService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -24,8 +25,9 @@ public class NBAController {
     private final NBAService service;
 
     @PostMapping(path = NBA_STANDINGS_ASK_PATH, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Answer> askQuestions(@RequestBody Question question) {
+    @ResponseStatus(HttpStatus.OK)
+    public Answer askQuestions(@RequestBody Question question) {
         log.info("GET {}", NBA_PATH + NBA_STANDINGS_ASK_PATH);
-        return ResponseEntity.ok(service.getAnswer(question));
+        return service.getAnswer(question);
     }
 }
